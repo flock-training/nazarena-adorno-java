100b7f54a20acd5c8f74377bfacb714c1b4e4dcd
Nazarena Adorno <naza.adorno@gmail.com>
2025-03-11 11:04:34 -0300

    add book class

---
+++ Training/src/main/java/Flock/Training/models/Book.java
@@ -0,0 +1,92 @@
+package Flock.Training.models;
+
+public class Book {
+
+    //Attributes
+    public String genre;
+    public String author;
+    public String image;
+    public String title;
+    public String subtitle;
+    public String publisher;
+    public String year;
+    public int pages;
+    public String isbn;
+
+    //Default constructor
+    public Book() {
+    }
+
+    //Getters y Setters
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+}
